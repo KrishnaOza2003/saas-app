3db105f6bb51a9d6b670358af469f39fb4fb29eb
KrishnaOza2003 <145345679+KrishnaOza2003@users.noreply.github.com>
2023-12-13 17:29:19 +0530

    Initial commit

---
+++ store/store.ts
@@ -0,0 +1,39 @@
+import { create } from "zustand";
+import { Subscription } from "@/types/Subscription";
+
+export type LanguagesSupported =
+  | "en"
+  | "es"
+  | "de"
+  | "fr"
+  | "hi"
+  | "gu"
+  | "ja"
+  | "it"
+  | "zh"
+  | "ar"
+  | "ru";
+
+export const LanguagesSupportedMap: Record<LanguagesSupported, string> = {
+  en: "English",
+  de: "German",
+  fr: "French",
+  es: "Spanish",
+  hi: "Hindi",
+  ja: "Japanese",
+  ru: "Russian",
+  zh: "Mandarin",
+  ar: "Arabic",
+  gu: "Gujarati",
+  it: "Italian",
+};
+
+interface SubscriptionState {
+  subscription: Subscription | null | undefined;
+  setSubscription: (subscription: Subscription | null) => void;
+}
+
+export const useSubscriptionStore = create<SubscriptionState>((set) => ({
+  subscription: undefined,
+  setSubscription: (subscription: Subscription | null) => set({ subscription }),
+}));
